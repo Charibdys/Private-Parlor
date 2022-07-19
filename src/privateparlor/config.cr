aa2b9b732c53c0e46ca90a0405353784911f962c
Charybdis <8596841-Charibdys@users.noreply.gitlab.com>
2022-07-18 22:19:29 -0400

    Refactor config and format files

---
+++ src/privateparlor/config.cr
@@ -0,0 +1,100 @@
+require "yaml"
+
+MESSAGE_ENTITIES = ["mention", "hashtag", "cashtag", "bot_command", "url", "email", "phone_number", "bold",
+                    "italic", "underline", "strikethrough", "spoiler", "code", "pre", "text_link", "text_mention"]
+
+module Configuration
+  extend self
+
+  class Config
+    include YAML::Serializable
+
+    @[YAML::Field(key: "api-token")]
+    getter token : String
+
+    @[YAML::Field(key: "database")]
+    getter database : String
+
+    @[YAML::Field(key: "log-level")]
+    getter log_level : Log::Severity = Log::Severity::Info
+
+    @[YAML::Field(key: "log-file")]
+    getter log_file : String? = nil
+
+    @[YAML::Field(key: "lifetime")]
+    getter lifetime : Int32 = 24
+
+    @[YAML::Field(key: "relay-luck")]
+    getter relay_luck : Bool = true
+
+    @[YAML::Field(key: "strip-format")]
+    getter entities : Array(String) = ["bold", "italic", "text_link"]
+
+    @[YAML::Field(key: "tripcode-salt")]
+    getter salt : String = ""
+
+    def initialize(@token : String, @database : String)
+    end
+  end
+
+  # Parse config.yaml and returns a `Config` object.
+  #
+  # Values that aren't specified in the config file will be set to a default value.
+  def parse_config : Config
+    begin
+      config = Config.from_yaml(File.open(File.expand_path("config.yaml")))
+      if check_config(config) == false
+        config = Config.new(config.token, config.database)
+      end
+
+      set_log(config)
+      return config
+    rescue ex : YAML::ParseException
+      Log.error(exception: ex) { "Could not parse the given value at row #{ex.line_number}. This could be because a required value was not set or the wrong type was given." }
+      exit
+    rescue ex : File::NotFoundError | File::AccessDeniedError
+      Log.error(exception: ex) { "Could not open \"./config.yaml\". Exiting..." }
+      exit
+    end
+  end
+
+  # Run additional checks on Config instance variables.
+  #
+  # Check bounds on `config.lifetime`.
+  # Check contents of `config.entities` for mispellings or duplicates.
+  def check_config(config : Config) : Bool
+    if (1..48).includes?(config.lifetime) == false
+      Log.notice { "Message lifetime not within range, was #{config.lifetime}; defaulting to 24 hours." }
+      return false
+    elsif (config.entities & MESSAGE_ENTITIES).size != config.entities.size
+      Log.notice { "Could not determine strip-format, was #{config.entities}; check for duplicates or mispellings. Using defaults." }
+      return false
+    end
+    return true
+  end
+
+  # Reset log with the severity level defined in `config.yaml`.
+  #
+  # A file can also be used to store log output. If the file does not exist, a new one will be made.
+  def set_log(config : Config) : Nil
+    # Skip setup if default values were given
+    if config.log_level == Log::Severity::Info && config.log_file == nil
+      return
+    end
+
+    # Reset log with log level; outputting to a file if a path was given
+    begin
+      if path = config.log_file
+        if File.file?(path) # If log file already exists
+          Log.setup(config.log_level, Log::IOBackend.new(File.open(path, "a+")))
+        else # Log file does not exist, make one
+          Log.setup(config.log_level, Log::IOBackend.new(File.new(path, "a+")))
+        end
+      else # Default to STDOUT
+        Log.setup(config.log_level)
+      end
+    rescue ex : File::NotFoundError | File::AccessDeniedError
+      Log.error(exception: ex) { "Could not open/create log file" }
+    end
+  end
+end
--- src/privateparlor/telegram.cr
+++ src/privateparlor/telegram.cr
@@ -1,6 +1,6 @@
 require "digest"
 
-# Bind to libcrypt 
+# Bind to libcrypt
 @[Link("crypt")]
 lib LibCrypt
   fun crypt(password : UInt8*, salt : UInt8*) : UInt8*
@@ -13,37 +13,28 @@ class PrivateParlor < Tourmaline::Client
   property replies : Replies
   property albums : Hash(String, Album)
   getter tasks : Hash(Symbol, Tasker::Task)
-  getter config : NamedTuple(
-    token: String,
-    database: String,
-    log_level: String,
-    log_path: String,
-    lifetime: Time::Span,
-    relay_luck: Bool,
-    entities: Array(String),
-    salt: String
-    )
+  getter config : Configuration::Config
 
   struct QueuedMessage
     getter hashcode : UInt64 | Array(UInt64)
     getter receiver : Int64
     getter reply_to : Int64 | Nil
     getter function : Proc(Int64, Int64 | Nil, Tourmaline::Message) | Proc(Int64, Int64 | Nil, Array(Tourmaline::Message))
-    
-    # Creates an instance of `QueuedMessage`. 
+
+    # Creates an instance of `QueuedMessage`.
     #
     # ## Arguments:
     #
-    # `hash` 
+    # `hash`
     # :     a hashcode that refers to the associated `MessageGroup` stored in the message history
     #
     # `receiver_id`
     # :     the ID of the user who will receive this message
     #
-    # `reply_msid` 
+    # `reply_msid`
     # :     the MSID of a message to reply to. May be `nil` if this message isn't a reply.
     #
-    # `function` 
+    # `function`
     # :     a proc that points to a Tourmaline CoreMethod send function and takes a user ID and MSID as its arguments
     def initialize(hash : UInt64 | Array(UInt64), receiver_id : Int64, reply_msid : Int64 | Nil, func : Proc)
       @hashcode = hash
@@ -51,7 +42,7 @@ class PrivateParlor < Tourmaline::Client
       @reply_to = reply_msid
       @function = func
     end
-  end 
+  end
 
   struct Album
     property message_ids : Array(Int64)
@@ -75,22 +66,21 @@ class PrivateParlor < Tourmaline::Client
   #
   # `connection`
   # :     the `DB::Databse` object obtained from the database path in the `config.yaml` file
-  def initialize(bot_token, config, connection, parse_mode)
-    super(bot_token: bot_token, default_parse_mode: parse_mode)
-    @config = config
-    @database = Database.new(connection)
-    @history = History.new(config[:lifetime])
+  def initialize(@config : Configuration::Config, parse_mode)
+    super(bot_token: config.token, default_parse_mode: parse_mode)
+    @database = Database.new(DB.open("sqlite3://#{Path.new(config.database)}")) # TODO: We'll want check if this works on Windows later
+    @history = History.new(config.lifetime.hours)
     @queue = Channel(QueuedMessage).new
-    @replies = Replies.new(config[:entities])
+    @replies = Replies.new(config.entities)
     @tasks = register_tasks()
     @albums = {} of String => Album
   end
 
   # Starts various background tasks and stores them in a hash.
-  def register_tasks() : Hash
+  def register_tasks : Hash
     tasks = {} of Symbol => Tasker::Task
     # Handle cache expiration
-    tasks.merge!({:cache => Tasker.every(((1/4) * @history.lifespan.to_i).hours) {@history.expire}})
+    tasks.merge!({:cache => Tasker.every(((1/4) * @history.lifespan.to_i).hours) { @history.expire }})
   end
 
   # Updates user's record in the database.
@@ -98,7 +88,7 @@ class PrivateParlor < Tourmaline::Client
     user.username = info.username
     user.realname = info.full_name
     user.set_active
-    database.modify_user(user) 
+    database.modify_user(user)
   end
 
   # User starts the bot and begins receiving messages.
@@ -126,7 +116,7 @@ class PrivateParlor < Tourmaline::Client
           send_message(user.id, @replies.already_in_chat)
         end
       else # User does not exist; add to DB
-        if(database.no_users?)
+        if (database.no_users?)
           user = database.add_user(info.id, info.username, info.full_name, rank: 1000)
         else
           user = database.add_user(info.id, info.username, info.full_name)
@@ -149,7 +139,7 @@ class PrivateParlor < Tourmaline::Client
       if (user = database.get_user(info.id)) && !user.left?
         user.set_left
         send_message(info.id, @replies.left)
-        database.modify_user(user) 
+        database.modify_user(user)
       end
     end
   end
@@ -174,14 +164,14 @@ class PrivateParlor < Tourmaline::Client
         end
 
         return send_message(user.id, @replies.user_info(
-            oid: user.get_obfuscated_id,
-            username: user.get_formatted_name,
-            rank: Ranks.new(user.rank),
-            karma: user.karma,
-            warnings: user.warnings,
-            warn_expiry: user.warn_expiry,
-            cooldown_until: user.cooldown_until
-          ))
+          oid: user.get_obfuscated_id,
+          username: user.get_formatted_name,
+          rank: Ranks.new(user.rank),
+          karma: user.karma,
+          warnings: user.warnings,
+          warn_expiry: user.warn_expiry,
+          cooldown_until: user.cooldown_until
+        ))
       end
     end
   end
@@ -192,10 +182,10 @@ class PrivateParlor < Tourmaline::Client
     if info = ctx.message.from.not_nil!
       if user = database.get_user(info.id)
         if reply = ctx.message.reply_message
-          if(@history.get_sender_id(reply.message_id) == user.id)
+          if (@history.get_sender_id(reply.message_id) == user.id)
             return send_message(user.id, @replies.upvoted_own_message)
           end
-          if(!@history.add_rating(reply.message_id, user.id))
+          if (!@history.add_rating(reply.message_id, user.id))
             return send_message(user.id, @replies.already_upvoted)
           end
 
@@ -203,7 +193,7 @@ class PrivateParlor < Tourmaline::Client
             reply_user.increment_karma
             database.modify_user(reply_user)
             send_message(user.id, @replies.gave_upvote)
-            if(!reply_user.hide_karma)
+            if (!reply_user.hide_karma)
               send_message(reply_user.id, @replies.got_upvote)
             end
           end
@@ -232,7 +222,7 @@ class PrivateParlor < Tourmaline::Client
     if info = ctx.message.from.not_nil!
       if user = database.get_user(info.id)
         if arg = get_args(ctx.message)
-          if !((index = arg.index('#')) && (0 < index < arg.size - 1)) || arg.includes?('\n')|| arg.size > 30
+          if !((index = arg.index('#')) && (0 < index < arg.size - 1)) || arg.includes?('\n') || arg.size > 30
             return send_message(info.id, @replies.invalid_tripcode_format)
           end
 
@@ -241,14 +231,12 @@ class PrivateParlor < Tourmaline::Client
 
           results = generate_tripcode(arg)
           return send_message(info.id, @replies.tripcode_set(results[:name], results[:tripcode]))
-          
         else
           return send_message(info.id, @replies.tripcode_info(user.tripcode))
         end
       end
     end
   end
-
 
   # Generate a 8chan or Secretlounge-ng style tripcode from a given string in the format `name#pass`.
   #
@@ -258,10 +246,10 @@ class PrivateParlor < Tourmaline::Client
     name = split[0]
     pass = split[1]
 
-    if !@config[:salt].empty?
+    if !@config.salt.empty?
       # Based on 8chan's secure tripcodes
-      salt = @config[:salt]
-      pass = String.new(pass.encode("Shift_JIS"), "Shift_JIS") 
+      salt = @config.salt
+      pass = String.new(pass.encode("Shift_JIS"), "Shift_JIS")
       tripcode = "!#{Digest::SHA1.base64digest(pass + salt)[0...10]}"
     else
       salt = (pass[...8] + "H.")[1...3]
@@ -328,7 +316,7 @@ class PrivateParlor < Tourmaline::Client
       end
     end
   end
-   
+
   # Promote a user to the administrator rank.
   @[Command(["admin"])]
   def admin_command(ctx)
@@ -400,7 +388,6 @@ class PrivateParlor < Tourmaline::Client
     end
   end
 
-
   # Remove a message from a user without giving a warning or cooldown.
   @[Command(["remove"])]
   def remove_message(ctx)
@@ -430,7 +417,7 @@ class PrivateParlor < Tourmaline::Client
   def delete_all_messages(ctx)
     if info = ctx.message.from.not_nil!
       if authorized?(info.id, Ranks::ADMIN)
-        if user = @database.get_blacklisted_users()
+        if user = @database.get_blacklisted_users
           delete_msids = [] of Int64
           user.each do |user|
             @history.get_msids_from_user(user.id).each do |msid|
@@ -447,7 +434,7 @@ class PrivateParlor < Tourmaline::Client
               @history.del_message_group(msid)
             end
 
-            return send_message(info.id, @replies.purge_complete(delete_msids.size()))
+            return send_message(info.id, @replies.purge_complete(delete_msids.size))
           end
         end
       end
@@ -505,7 +492,7 @@ class PrivateParlor < Tourmaline::Client
         if !((text = message.text) && text.starts_with?('/')) # Don't relay commands
           # NOTE: If a user sends too many messages at once, this may lock the database when relaying messages
           update_user(info, user)
-          if(check_message_type(message, info))
+          if (check_message_type(message, info))
             hash = @history.new_message(info.id, message.message_id)
             relay(message, info, hash)
           end
@@ -516,13 +503,12 @@ class PrivateParlor < Tourmaline::Client
     end
   end
 
-
   # Check whether or not the type of message should be relayed
   # Prevents non-anonymous polls and handles Album relaying
   #
   # Returns true if the message type is allowed, false otherwise
   def check_message_type(message, info) : Bool
-    if poll = message.poll 
+    if poll = message.poll
       if poll.is_anonymous == false
         send_message(info.id, @replies.deanon_poll)
         return false
@@ -538,18 +524,18 @@ class PrivateParlor < Tourmaline::Client
         entities = @replies.remove_entities(entities)
       end
 
-      if (media = message.photo.last?) 
-        input = InputMediaPhoto.new(media.file_id, caption: caption , caption_entities: entities)
-      elsif (media = message.video) 
+      if (media = message.photo.last?)
+        input = InputMediaPhoto.new(media.file_id, caption: caption, caption_entities: entities)
+      elsif (media = message.video)
         input = InputMediaVideo.new(media.file_id, caption: caption, caption_entities: entities)
-      elsif (media = message.audio) 
+      elsif (media = message.audio)
         input = InputMediaAudio.new(media.file_id, caption: caption, caption_entities: entities)
       elsif (media = message.document)
         input = InputMediaDocument.new(media.file_id, caption: caption, caption_entities: entities)
       else
         return false
       end
-        
+
       if @albums[album]?
         @albums[album].message_ids << message.message_id
         @albums[album].media_ids << input
@@ -575,65 +561,65 @@ class PrivateParlor < Tourmaline::Client
   # Takes a message and returns a CoreMethod proc according to its content type.
   def type_to_proc(message) : Proc(Int64, Int64 | Nil, Tourmaline::Message) | Proc(Int64, Int64 | Nil, Array(Tourmaline::Message)) | Nil
     if (forward = message.forward_from) || (forward = message.forward_from_chat)
-      return proc = ->(receiver : Int64, reply : Int64 | Nil){forward_message(receiver, message.chat.id, message.message_id)}
+      return proc = ->(receiver : Int64, reply : Int64 | Nil) { forward_message(receiver, message.chat.id, message.message_id) }
     end
 
     if album = message.media_group_id
       if temp_album = @albums.delete(album)
-        return proc = ->(receiver : Int64, reply : Int64 | Nil){send_media_group(receiver, temp_album.media_ids, reply_to_message: reply)}
+        return proc = ->(receiver : Int64, reply : Int64 | Nil) { send_media_group(receiver, temp_album.media_ids, reply_to_message: reply) }
       else
         return nil
       end
     end
-    
+
     if caption = message.caption
       caption = @replies.strip_format(caption, message.caption_entities)
     end
 
     # Standard text message
     if text = message.text
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_message(receiver, @replies.strip_format(text, message.entities), reply_to_message: reply)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_message(receiver, @replies.strip_format(text, message.entities), reply_to_message: reply) }
 
-    # Captioned types
+      # Captioned types
     elsif animation = message.animation
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_animation(receiver, animation.file_id, caption: caption, reply_to_message: reply)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_animation(receiver, animation.file_id, caption: caption, reply_to_message: reply) }
     elsif audio = message.audio
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_audio(receiver, audio.file_id, caption, reply_to_message: reply)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_audio(receiver, audio.file_id, caption, reply_to_message: reply) }
     elsif document = message.document
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_document(receiver, document.file_id, caption, reply_to_message: reply)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_document(receiver, document.file_id, caption, reply_to_message: reply) }
     elsif video = message.video
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_video(receiver, video.file_id, caption: caption, reply_to_message: reply)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_video(receiver, video.file_id, caption: caption, reply_to_message: reply) }
     elsif video_note = message.video_note
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_video_note(receiver, video_note.file_id, caption: caption, reply_to_message: reply)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_video_note(receiver, video_note.file_id, caption: caption, reply_to_message: reply) }
     elsif voice = message.voice
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_voice(receiver, voice.file_id, caption, reply_to_message: reply)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_voice(receiver, voice.file_id, caption, reply_to_message: reply) }
     elsif photo = message.photo.last? # The last photo in the array will have the highest resolution
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_photo(receiver, photo.file_id, caption, reply_to_message: reply)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_photo(receiver, photo.file_id, caption, reply_to_message: reply) }
 
-    # Forward polls
+      # Forward polls
     elsif poll = message.poll
-      proc = ->(receiver : Int64, reply : Int64 | Nil){forward_message(receiver, message.chat.id, message.message_id)}
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { forward_message(receiver, message.chat.id, message.message_id) }
 
-    # Stickers
+      # Stickers
     elsif sticker = message.sticker
-      proc = ->(receiver : Int64, reply : Int64 | Nil){send_sticker(receiver, sticker.file_id, reply_to_message: reply)}
-    
-    # Dice and other luck types
+      proc = ->(receiver : Int64, reply : Int64 | Nil) { send_sticker(receiver, sticker.file_id, reply_to_message: reply) }
+
+      # Dice and other luck types
     elsif dice = message.dice
-      if @config[:relay_luck]
+      if config.relay_luck
         case dice.emoji
         when "🎲"
-          proc = ->(receiver : Int64, reply : Int64 | Nil){send_dice(receiver, message.text, reply_to_message: reply)}
+          proc = ->(receiver : Int64, reply : Int64 | Nil) { send_dice(receiver, message.text, reply_to_message: reply) }
         when "🎯"
-          proc = ->(receiver : Int64, reply : Int64 | Nil){send_dart(receiver, message.text, reply_to_message: reply)}
+          proc = ->(receiver : Int64, reply : Int64 | Nil) { send_dart(receiver, message.text, reply_to_message: reply) }
         when "🏀"
-          proc = ->(receiver : Int64, reply : Int64 | Nil){send_basketball(receiver, message.text, reply_to_message: reply)}
+          proc = ->(receiver : Int64, reply : Int64 | Nil) { send_basketball(receiver, message.text, reply_to_message: reply) }
         when "⚽"
-          proc = ->(receiver : Int64, reply : Int64 | Nil){send_soccerball(receiver, message.text, reply_to_message: reply)}
+          proc = ->(receiver : Int64, reply : Int64 | Nil) { send_soccerball(receiver, message.text, reply_to_message: reply) }
         when "🎰"
-          proc = ->(receiver : Int64, reply : Int64 | Nil){send_slot_machine(receiver, message.text, reply_to_message: reply)}
+          proc = ->(receiver : Int64, reply : Int64 | Nil) { send_slot_machine(receiver, message.text, reply_to_message: reply) }
         when "🎳"
-          proc = ->(receiver : Int64, reply : Int64 | Nil){send_bowling(receiver, message.text, reply_to_message: reply)}
+          proc = ->(receiver : Int64, reply : Int64 | Nil) { send_bowling(receiver, message.text, reply_to_message: reply) }
         end
       else
         proc = nil
@@ -646,7 +632,7 @@ class PrivateParlor < Tourmaline::Client
   # Relay message to every joined user except for the sender.
   def relay(message, info, hash)
     if proc = type_to_proc(message)
-      if !(reply = message.reply_message) # Message was NOT a reply
+      if !(reply = message.reply_message)                     # Message was NOT a reply
         @database.get_prioritized_users.each do |receiver_id| # No need for a left? check here
           if receiver_id != info.id
             add_to_queue(hash, receiver_id, nil, proc)
@@ -661,14 +647,14 @@ class PrivateParlor < Tourmaline::Client
         end
       end
     else
-      Log.error {"Could not create proc for message type. Message was #{message}"}
+      Log.error { "Could not create proc for message type. Message was #{message}" }
     end
   end
 
   # Returns arguments found after a command from a message text.
   def get_args(msg : Tourmaline::Message, count : Int = 1) : String | Array(String) | Nil
     args = msg.text.not_nil!.split(count + 1)
-    case args.size()
+    case args.size
     when 2
       return args[1]
     when 2..
@@ -687,7 +673,7 @@ class PrivateParlor < Tourmaline::Client
     @queue.send(QueuedMessage.new(hashcode, receiver_id, reply_msid, func))
   end
 
-  # Receives a `Message` from the `queue` channel, calls its proc, and adds the 
+  # Receives a `Message` from the `queue` channel, calls its proc, and adds the
   # returned message id to the History
   #
   # This function should be invoked in a Fiber
@@ -700,20 +686,19 @@ class PrivateParlor < Tourmaline::Client
     if !success.is_a?(Array(Tourmaline::Message))
       @history.add_to_cache(msg.hashcode.as(UInt64), success.message_id, msg.receiver)
     else
-      sent_msids = success.map {|msg| msg.message_id}
+      sent_msids = success.map { |msg| msg.message_id }
 
       sent_msids.zip(msg.hashcode.as(Array(UInt64))) do |msid, hashcode|
         @history.add_to_cache(hashcode, msid, msg.receiver)
       end
     end
   end
-
 end
 
 enum Ranks
-  BANNED  = -10
-  USER    = 0
-  MOD     = 10
-  ADMIN   = 100
-  HOST    = 1000 
-end 
+  BANNED =  -10
+  USER   =    0
+  MOD    =   10
+  ADMIN  =  100
+  HOST   = 1000
+end
