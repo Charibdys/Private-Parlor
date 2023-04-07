7e0c1a4ed35f067a3138cc0145f4adae1de7d027
Charibdys <87929585+Charibdys@users.noreply.github.com>
2023-04-07 15:26:58 -0400

    Fix deleting orignal message (cache history)

--- src/privateparlor/history.cr
+++ src/privateparlor/history.cr
@@ -43,7 +43,7 @@ abstract class History
   # Returns the receivers hash found in the associated `MessageGroup`
   def get_all_msids(msid : Int64) : Hash
     if msg = @msid_map[msid]?
-      {msg.sender => msg.origin_msid}.merge!(msg.receivers)
+      msg.receivers
     else
       {} of Int64 => Int64
     end
